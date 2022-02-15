bde6ea4435b42cbe5ec79c3c484244ef6b86b589
Sage Hogue <sagehogue@gmail.com>
2022-02-15 14:44:48 -0800

    emails work, styling them now

---
+++ functions/HTMLStyles.js
@@ -0,0 +1,21 @@
+const donation = {
+  // styling for elements in donor receipt email
+  payer: {},
+  //   styling for elements for donation recipient
+  payee: {},
+  // shared styling
+  background: `background-color: #282c34;
+  width: 100vw;
+  min-height: 100vh;
+  display: flex;`,
+  form: ``,
+  header: ``,
+  h1: ``,
+  hr1: ``,
+  hr2: ``,
+  li: ``,
+  p: ``,
+  ul: ``,
+};
+
+module.exports = { donation };
--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,8 @@
 const functions = require("firebase-functions");
 const nodemailer = require("nodemailer");
 
+const HTMLStyles = require("./HTMLStyles");
+
 const firebaseAdmin = require("firebase-admin");
 getFirestore = () => firebaseAdmin.firestore();
 initializeApp = () => firebaseAdmin.initializeApp();
@@ -93,45 +95,49 @@ exports.sendEmail = functions.firestore
       subject: `New Donation of ${snap.data().total} ${
         snap.data().purchase_units[0].amount.currency_code
       }`,
-      html: `<h1>New Donation of ${snap.data().total}</h1>
-    <p> ${snap.data().payer.name.given_name} ${
+      html: `<div style={${HTMLStyles.donation.background}}><h1 style={${
+        HTMLStyles.donation.h1
+      }}>New Donation of ${snap.data().total}</h1>
+    <p style={${HTMLStyles.donation.p}}> ${snap.data().payer.name.given_name} ${
         snap.data().payer.name.surname
       } kindly donated ${
         snap.data().total
       } with the following distribution at ${snap.data().time}. </p>
-    <ul>
+    <ul style={${HTMLStyles.donation.ul}}>
     <li><b>General Offering: </b>${snap.data().donation.GO} ${
         snap.data().purchase_units[0].amount.currency_code
       }</li>
-    <li><b>Tithes: </b>${snap.data().donation.TI} ${
-        snap.data().purchase_units[0].amount.currency_code
-      }</li>
-    <li><b>Youth Ministry: </b>${snap.data().donation.YM} ${
-        snap.data().purchase_units[0].amount.currency_code
-      }</li>
-    <li><b>Women's Ministry: </b>${snap.data().donation.WM} ${
-        snap.data().purchase_units[0].amount.currency_code
-      }</li>
+    <li style={${HTMLStyles.donation.li}}><b>Tithes: </b>${
+        snap.data().donation.TI
+      } ${snap.data().purchase_units[0].amount.currency_code}</li>
+    <li style={${HTMLStyles.donation.li}}><b>Youth Ministry: </b>${
+        snap.data().donation.YM
+      } ${snap.data().purchase_units[0].amount.currency_code}</li>
+    <li style={${HTMLStyles.donation.li}}><b>Women's Ministry: </b>${
+        snap.data().donation.WM
+      } ${snap.data().purchase_units[0].amount.currency_code}</li>
 
-<li><b>Option1: </b>${snap.data().donation.O1} ${
-        snap.data().purchase_units[0].amount.currency_code
-      }</li>
-<li><b>Option2: </b>${snap.data().donation.O2} ${
-        snap.data().purchase_units[0].amount.currency_code
-      }</li>
-<li><b>Option3: </b>${snap.data().donation.O3} ${
-        snap.data().purchase_units[0].amount.currency_code
-      }</li>
-<li><b>Option4: </b>${snap.data().donation.O4} ${
-        snap.data().purchase_units[0].amount.currency_code
-      }</li>
-<li><b>Other: </b>${snap.data().donation.OT} ${
-        snap.data().purchase_units[0].amount.currency_code
-      }</li>
+<li style={${HTMLStyles.donation.li}}><b>Option1: </b>${
+        snap.data().donation.O1
+      } ${snap.data().purchase_units[0].amount.currency_code}</li>
+<li style={${HTMLStyles.donation.li}}><b>Option2: </b>${
+        snap.data().donation.O2
+      } ${snap.data().purchase_units[0].amount.currency_code}</li>
+<li style={${HTMLStyles.donation.li}}><b>Option3: </b>${
+        snap.data().donation.O3
+      } ${snap.data().purchase_units[0].amount.currency_code}</li>
+<li style={${HTMLStyles.donation.li}}><b>Option4: </b>${
+        snap.data().donation.O4
+      } ${snap.data().purchase_units[0].amount.currency_code}</li>
+<li style={${HTMLStyles.donation.li}}><b>Other: </b>${
+        snap.data().donation.OT
+      } ${snap.data().purchase_units[0].amount.currency_code}</li>
 </ul>
-<p>Donation comment: ${snap.data().comment}</p>
-     <p> <b>Donor Email: </b>${snap.data().payer.email_address} </p>
-     `,
+<p style={${HTMLStyles.donation.p}}>Donation comment: ${snap.data().comment}</p>
+     <p style={${HTMLStyles.donation.p}}> <b>Donor Email: </b>${
+        snap.data().payer.email_address
+      } </p>
+     </div>`,
     };
     return transporter.sendMail(mailOptions, (error, data) => {
       if (error) {
